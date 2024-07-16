782ab72b051503756343fb995e211bdc92b286b1
Kaleb Fox <kalebfox7@yahoo.com>
2024-07-16 16:23:04 -0400

    done

--- server/app.py
+++ server/app.py
@@ -1,10 +1,11 @@
 # server/app.py
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, make_response, request
 from flask_migrate import Migrate
-
+from flask_restful import Resource, Api
 from models import db, Earthquake
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
@@ -13,7 +14,7 @@ app.json.compact = False
 
 migrate = Migrate(app, db)
 db.init_app(app)
-
+api=Api(app=app)
 
 @app.route('/')
 def index():
@@ -21,7 +22,35 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+class EarthquakeResource(Resource):
+    def get(self, id):
+        
+        earthquake = Earthquake.query.get(id)
+        if earthquake:
+            return earthquake.to_dict(), 200
+        else:
+            return {"message": f'Earthquake {id} not found.'}, 404
+        
+class EarthquakemagResource(Resource):
+    def get(self, magnitude):                      
+        earthquakes = Earthquake.query.filter(Earthquake.magnitude > magnitude).all()
+        #maybe len of earthquake?
+        count = len(earthquakes)
+        body = {
+                "count": count,
+                "quakes": [{
+                "id": earthquake.id,
+                "location": earthquake.location,
+                "magnitude": earthquake.magnitude,
+                "year": earthquake.year
+                } for earthquake in earthquakes]
+                }
+        return body, 200
+        
 
+
+api.add_resource(EarthquakemagResource, "/earthquakes/magnitude/<float:magnitude>")
+api.add_resource(EarthquakeResource, "/earthquakes/<id>")
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
